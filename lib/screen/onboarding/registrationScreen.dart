9b3ab4417267f67f234f384b130f48d1734731e4
imamulanan <anan.imamul@gmail.com>
2025-08-07 00:43:16 +0600

    Initial commit

--- lib/screen/onboarding/registrationScreen.dart
+++ lib/screen/onboarding/registrationScreen.dart
@@ -9,48 +9,47 @@ class registrationScreen extends StatefulWidget {
 }
 
 class _registrationScreenState extends State<registrationScreen> {
+  Map<String, String> FormValues = {
+    "email": "",
+    "firstName": "",
+    "lastName": "",
+    "mobile": "",
+    "password": "",
+    "photo": "",
+    "cpassword": "",
+  };
+  bool Loading = false;
 
-  Map<String,String> FormValues={"email":"", "firstName":"","lastName":"","mobile":"","password":"","photo":"","cpassword":""};
-  bool Loading=false;
-
-  InputOnChange(MapKey, Textvalue){
+  InputOnChange(MapKey, Textvalue) {
     setState(() {
       FormValues.update(MapKey, (value) => Textvalue);
     });
   }
 
-  FormOnSubmit() async{
-
-    if(FormValues['email']!.length==0){
+  FormOnSubmit() async {
+    if (FormValues['email']!.length == 0) {
       ErrorToast('Email Required !');
-    }
-
-    else if(FormValues['firstName']!.length==0){
+    } else if (FormValues['firstName']!.length == 0) {
       ErrorToast('First Name Required !');
-    }
-
-    else if(FormValues['lastName']!.length==0){
+    } else if (FormValues['lastName']!.length == 0) {
       ErrorToast('Last Name Required !');
-    }
-
-    else if(FormValues['mobile']!.length==0){
+    } else if (FormValues['mobile']!.length == 0) {
       ErrorToast('Mobile No Required !');
-    }
-
-    else if(FormValues['password']!.length==0){
+    } else if (FormValues['password']!.length == 0) {
       ErrorToast('Mobile No Required !');
-    }
-    else if(FormValues['password']!=FormValues['cpassword']){
+    } else if (FormValues['password'] != FormValues['cpassword']) {
       ErrorToast('Confirm password should be same!');
-    }
-    else{
-      setState(() {Loading=true;});
-      bool res=await RegistrationRequest(FormValues);
-      if(res==true){
+    } else {
+      setState(() {
+        Loading = true;
+      });
+      bool res = await RegistrationRequest(FormValues);
+      if (res == true) {
         Navigator.pushNamedAndRemoveUntil(context, "/login", (route) => false);
-      }
-      else{
-        setState(() {Loading=false;});
+      } else {
+        setState(() {
+          Loading = false;
+        });
       }
     }
   }
@@ -64,8 +63,9 @@ class _registrationScreenState extends State<registrationScreen> {
           Container(
             alignment: Alignment.center,
             child: SingleChildScrollView(
-                child:Loading?(Center(child: CircularProgressIndicator())):(
-                    Container(
+              child: Loading
+                  ? (Center(child: CircularProgressIndicator()))
+                  : (Container(
                       padding: EdgeInsets.all(30),
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
@@ -73,72 +73,74 @@ class _registrationScreenState extends State<registrationScreen> {
                         children: [
                           Text("Join With Us", style: Head1Text(colorDarkBlue)),
                           SizedBox(height: 1),
-                          Text("Learn with Imamul Anan", style: Head6Text(colorLightGray)),
-
-                          SizedBox(height: 20),
-                          TextFormField(
-                              decoration: AppInputDecoration("Email Address"),
-                              onChanged: (Textvalue){
-                                InputOnChange("email",Textvalue);
-                              }
+                          Text(
+                            "Learn with Imamul Anan",
+                            style: Head6Text(colorLightGray),
                           ),
 
                           SizedBox(height: 20),
                           TextFormField(
-                              decoration: AppInputDecoration("First Name"),
-                              onChanged: (Textvalue){
-                                InputOnChange("firstName",Textvalue);
-                              }
-                          ),
-
-                          SizedBox(height: 20),
-                          TextFormField(
-                              decoration: AppInputDecoration("Last Name"),
-                              onChanged: (Textvalue){
-                                InputOnChange("lastName",Textvalue);
-                              }
-                          ),
-
-                          SizedBox(height: 20),
-                          TextFormField(
-                              decoration: AppInputDecoration("Mobile"),
-                              onChanged: (Textvalue){
-                                InputOnChange("mobile",Textvalue);
-                              }
-                          ),
-
-                          SizedBox(height: 20),
-                          TextFormField(
-                              decoration: AppInputDecoration("Password"),
-                              onChanged: (Textvalue){
-                                InputOnChange("password",Textvalue);
-                              }
-                          ),
-
-
-                          SizedBox(height: 20),
-                          TextFormField(
-                              decoration: AppInputDecoration("Confirm Password"),
-                              onChanged: (Textvalue){
-                                InputOnChange("cpassword",Textvalue);
-                              }
-                          ),
-
-                          SizedBox(height: 20),
-                          Container(child: ElevatedButton(
-                            style: AppButtonStyle(),
-                            child: SuccessButtonChild('Registration'),
-                            onPressed: (){
-                              FormOnSubmit();
+                            decoration: AppInputDecoration("Email Address"),
+                            onChanged: (Textvalue) {
+                              InputOnChange("email", Textvalue);
                             },
-                          ),)
+                          ),
 
+                          SizedBox(height: 20),
+                          TextFormField(
+                            decoration: AppInputDecoration("First Name"),
+                            onChanged: (Textvalue) {
+                              InputOnChange("firstName", Textvalue);
+                            },
+                          ),
+
+                          SizedBox(height: 20),
+                          TextFormField(
+                            decoration: AppInputDecoration("Last Name"),
+                            onChanged: (Textvalue) {
+                              InputOnChange("lastName", Textvalue);
+                            },
+                          ),
+
+                          SizedBox(height: 20),
+                          TextFormField(
+                            decoration: AppInputDecoration("Mobile"),
+                            onChanged: (Textvalue) {
+                              InputOnChange("mobile", Textvalue);
+                            },
+                          ),
+
+                          SizedBox(height: 20),
+                          TextFormField(
+                            decoration: AppInputDecoration("Password"),
+                            onChanged: (Textvalue) {
+                              InputOnChange("password", Textvalue);
+                            },
+                          ),
+
+                          SizedBox(height: 20),
+                          TextFormField(
+                            decoration: AppInputDecoration("Confirm Password"),
+                            onChanged: (Textvalue) {
+                              InputOnChange("cpassword", Textvalue);
+                            },
+                          ),
+
+                          SizedBox(height: 20),
+                          Container(
+                            child: ElevatedButton(
+                              style: AppButtonStyle(),
+                              child: SuccessButtonChild('Registration'),
+                              onPressed: () {
+                                FormOnSubmit();
+                              },
+                            ),
+                          ),
                         ],
                       ),
-                    )
-                )
+                    )),
             ),
-          )
+          ),
         ],
       ),
     );
