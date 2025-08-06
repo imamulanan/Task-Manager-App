3fff87166546026b96119830cab151fc9d1d386d
imamulanan <anan.imamul@gmail.com>
2025-08-07 01:04:38 +0600

    Initial commit

--- lib/screen/onboarding/emailVerificationScreen.dart
+++ lib/screen/onboarding/emailVerificationScreen.dart
@@ -2,35 +2,38 @@ import 'package:flutter/material.dart';
 
 import '../../api/apiClient.dart';
 import '../../style/style.dart';
+
 class emailVerificationScreen extends StatefulWidget {
   const emailVerificationScreen({Key? key}) : super(key: key);
   @override
-  State<emailVerificationScreen> createState() => _emailVerificationScreenState();
+  State<emailVerificationScreen> createState() =>
+      _emailVerificationScreenState();
 }
 
 class _emailVerificationScreenState extends State<emailVerificationScreen> {
+  Map<String, String> FormValues = {"email": ""};
+  bool Loading = false;
 
-  Map<String,String> FormValues={"email":""};
-  bool Loading=false;
-
-  InputOnChange(MapKey, Textvalue){
+  InputOnChange(MapKey, Textvalue) {
     setState(() {
       FormValues.update(MapKey, (value) => Textvalue);
     });
   }
 
-  FormOnSubmit() async{
-    if(FormValues['email']!.length==0){
+  FormOnSubmit() async {
+    if (FormValues['email']!.length == 0) {
       ErrorToast('Email Required !');
-    }
-    else{
-      setState((){Loading=true;});
-      bool res=await VerifyEmailRequest(FormValues['email']);
-      if(res==true){
+    } else {
+      setState(() {
+        Loading = true;
+      });
+      bool res = await VerifyEmailRequest(FormValues['email']);
+      if (res == true) {
         Navigator.pushNamed(context, "/pinVerification");
-      }
-      else{
-        setState(() {Loading=false;});
+      } else {
+        setState(() {
+          Loading = false;
+        });
       }
     }
   }
@@ -42,37 +45,45 @@ class _emailVerificationScreenState extends State<emailVerificationScreen> {
         children: [
           ScreenBackground(context),
           Container(
-              alignment: Alignment.center,
-              child: Loading?(Center(child: CircularProgressIndicator())):(
-                  SingleChildScrollView(
+            alignment: Alignment.center,
+            child: Loading
+                ? (Center(child: CircularProgressIndicator()))
+                : (SingleChildScrollView(
                     padding: EdgeInsets.all(30),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text("Your Email Address", style: Head1Text(colorDarkBlue)),
+                        Text(
+                          "Your Email Address",
+                          style: Head1Text(colorDarkBlue),
+                        ),
                         SizedBox(height: 1),
-                        Text("A 6 digit verification pin will send to your email address", style: Head6Text(colorLightGray)),
+                        Text(
+                          "A 6 digit verification pin will send to your email address",
+                          style: Head6Text(colorLightGray),
+                        ),
                         SizedBox(height: 20),
                         TextFormField(
-                          onChanged: (Textvalue){
-                            InputOnChange("email",Textvalue);
+                          onChanged: (Textvalue) {
+                            InputOnChange("email", Textvalue);
                           },
                           decoration: AppInputDecoration("Email Address"),
                         ),
                         SizedBox(height: 20),
-                        Container(child: ElevatedButton(
-                          style: AppButtonStyle(),
-                          child: SuccessButtonChild('Next'),
-                          onPressed: (){
-                            FormOnSubmit();
-                          },
-                        ),)
+                        Container(
+                          child: ElevatedButton(
+                            style: AppButtonStyle(),
+                            child: SuccessButtonChild('Next'),
+                            onPressed: () {
+                              FormOnSubmit();
+                            },
+                          ),
+                        ),
                       ],
                     ),
-                  )
-              )
-          )
+                  )),
+          ),
         ],
       ),
     );
