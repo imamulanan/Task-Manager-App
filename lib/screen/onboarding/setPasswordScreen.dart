5fff808dbd2c1b8910a6b63839eec50be35c2082
imamulanan <anan.imamul@gmail.com>
2025-08-07 14:23:49 +0600

    Update commit

--- lib/screen/onboarding/setPasswordScreen.dart
+++ lib/screen/onboarding/setPasswordScreen.dart
@@ -4,19 +4,19 @@ import '../../style/style.dart';
 import '../../utility/utility.dart';
 
 class setPasswordScreen extends StatefulWidget {
-
   const setPasswordScreen({Key? key}) : super(key: key);
   @override
   State<setPasswordScreen> createState() => _setPasswordScreenState();
-
 }
 
-
 class _setPasswordScreenState extends State<setPasswordScreen> {
-
-
-  Map<String,String> FormValues={"email":"", "OTP":"","password":"","cpassword":""};
-  bool Loading=false;
+  Map<String, String> FormValues = {
+    "email": "",
+    "OTP": "",
+    "password": "",
+    "cpassword": "",
+  };
+  bool Loading = false;
 
   @override
   initState() {
@@ -25,33 +25,34 @@ class _setPasswordScreenState extends State<setPasswordScreen> {
   }
 
   callStoreData() async {
-    String? OTP= await ReadUserData("OTPVerification");
-    String? Email= await ReadUserData("EmailVerification");
+    String? OTP = await ReadUserData("OTPVerification");
+    String? Email = await ReadUserData("EmailVerification");
     InputOnChange("email", Email);
     InputOnChange("OTP", OTP);
   }
 
-  InputOnChange(MapKey, Textvalue){
+  InputOnChange(MapKey, Textvalue) {
     setState(() {
       FormValues.update(MapKey, (value) => Textvalue);
     });
   }
 
-  FormOnSubmit() async{
-    if(FormValues['password']!.length==0){
+  FormOnSubmit() async {
+    if (FormValues['password']!.length == 0) {
       ErrorToast('Password Required !');
-    }
-    else if(FormValues['password']!=FormValues['cpassword']){
+    } else if (FormValues['password'] != FormValues['cpassword']) {
       ErrorToast('Confirm password should be same!');
-    }
-    else{
-      setState(() {Loading=true;});
-      bool res=await SetPasswordRequest(FormValues);
-      if(res==true){
+    } else {
+      setState(() {
+        Loading = true;
+      });
+      bool res = await SetPasswordRequest(FormValues);
+      if (res == true) {
         Navigator.pushNamedAndRemoveUntil(context, "/login", (route) => false);
-      }
-      else{
-        setState(() {Loading=false;});
+      } else {
+        setState(() {
+          Loading = false;
+        });
       }
     }
   }
@@ -64,41 +65,48 @@ class _setPasswordScreenState extends State<setPasswordScreen> {
           ScreenBackground(context),
           Container(
             alignment: Alignment.center,
-            child: Loading?(Center(child: CircularProgressIndicator())):(SingleChildScrollView(
-              padding: EdgeInsets.all(30),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("Set Password", style: Head1Text(colorDarkBlue)),
-                  SizedBox(height: 1),
-                  Text("Minimum length password 8 character with Latter and number combination ", style: Head7Text(colorLightGray)),
-                  SizedBox(height: 20),
-                  TextFormField(
-                    onChanged: (Textvalue){
-                      InputOnChange("password",Textvalue);
-                    },
-                    decoration: AppInputDecoration("Password"),
-                  ),
-                  SizedBox(height: 20),
-                  TextFormField(
-                    onChanged: (Textvalue){
-                      InputOnChange("cpassword",Textvalue);
-                    },
-                    decoration: AppInputDecoration("Confirm Password"),
-                  ),
-                  SizedBox(height: 20),
-                  Container(child: ElevatedButton(
-                    style: AppButtonStyle(),
-                    child: SuccessButtonChild('Confirm'),
-                    onPressed: (){
-                      FormOnSubmit();
-                    },
-                  ),)
-                ],
-              ),
-            )),
-          )
+            child: Loading
+                ? (Center(child: CircularProgressIndicator()))
+                : (SingleChildScrollView(
+                    padding: EdgeInsets.all(30),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Set Password", style: Head1Text(colorDarkBlue)),
+                        SizedBox(height: 1),
+                        Text(
+                          "Minimum length password 8 character with Latter and number combination ",
+                          style: Head7Text(colorLightGray),
+                        ),
+                        SizedBox(height: 20),
+                        TextFormField(
+                          onChanged: (Textvalue) {
+                            InputOnChange("password", Textvalue);
+                          },
+                          decoration: AppInputDecoration("Password"),
+                        ),
+                        SizedBox(height: 20),
+                        TextFormField(
+                          onChanged: (Textvalue) {
+                            InputOnChange("cpassword", Textvalue);
+                          },
+                          decoration: AppInputDecoration("Confirm Password"),
+                        ),
+                        SizedBox(height: 20),
+                        Container(
+                          child: ElevatedButton(
+                            style: AppButtonStyle(),
+                            child: SuccessButtonChild('Confirm'),
+                            onPressed: () {
+                              FormOnSubmit();
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  )),
+          ),
         ],
       ),
     );
