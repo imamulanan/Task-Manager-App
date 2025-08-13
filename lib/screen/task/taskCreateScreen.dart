7801641bc76dda8c1a2ebb3fb22f64c18c040642
imamulanan <anan.imamul@gmail.com>
2025-08-13 13:31:24 +0600

    Update commit

--- lib/screen/task/taskCreateScreen.dart
+++ lib/screen/task/taskCreateScreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../../Style/Style.dart';
+import '../../api/apiClient.dart';
 
 class taskCreateScreen extends StatefulWidget {
   const taskCreateScreen({super.key});
@@ -11,6 +12,35 @@ class taskCreateScreen extends StatefulWidget {
 }
 
 class _taskCreateScreenState extends State<taskCreateScreen> {
+
+  Map<String,String> FormValues={"title":"", "description":"","status":"New"};
+  bool Loading=false;
+
+  InputOnChange(MapKey, Textvalue){
+    setState(() {
+      FormValues.update(MapKey, (value) => Textvalue);
+    });
+  }
+
+  FormOnSubmit() async{
+    if(FormValues['title']!.length==0){
+      ErrorToast('Title Required !');
+    }
+    else if(FormValues['description']!.length==0){
+      ErrorToast('Description Required !');
+    }
+    else{
+      setState(() {Loading=true;});
+      bool res=await TaskCreateRequest(FormValues);
+      if(res==true){
+        Navigator.pushNamedAndRemoveUntil(context, "/", (route) => false);
+      }
+      else{
+        setState(() {Loading=false;});
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
