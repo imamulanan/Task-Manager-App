7cc09fe3f93bdbeb583e7b70b28ca89904f38b23
imamulanan <anan.imamul@gmail.com>
2025-08-13 13:27:57 +0600

    Update commit

--- lib/screen/task/taskCreateScreen.dart
+++ lib/screen/task/taskCreateScreen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../Style/Style.dart';
 
 class taskCreateScreen extends StatefulWidget {
   const taskCreateScreen({super.key});
@@ -10,6 +13,8 @@ class taskCreateScreen extends StatefulWidget {
 class _taskCreateScreenState extends State<taskCreateScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(backgroundColor: colorGreen, title: Text('Create New Task')),
+    );
   }
 }
